9220ac0b9225414a380205c99fd2b3a6d1ffeb9a
soyoung677 <lsy141@gmail.com>
2018-02-13 16:09:58 -0800

    sql query

---
+++ insert_data.sql
@@ -0,0 +1,4 @@
+insert into test.nation_capital (nation, capital) values('South Korea', 'Seoul');
+insert into test.nation_capital (nation, capital) values('Japan', 'Tokyo');
+insert into test.nation_capital (nation, capital) values('England', 'London');
+insert into test.nation_capital (nation, capital) values('France', 'Paris');
